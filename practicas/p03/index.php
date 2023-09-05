be41bdf77f291d5efb26da05415f3624f04030b0
CrisTlalmis <cristlalmis@gmail.com>
2023-09-05 12:45:50 -0600

    Ejercicio 1 completado

---
+++ practicas/p03/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.1//EN”
+“http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd”>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica 3</title>
+</head>
+<body>
+    <h2>Ejercicio 1</h2>
+    <p>Determina cuál de las siguientes variables son válidas y explica por qué:</p>
+     <p>$_myvar, $_7var, myvar, $myvar, $var7, $_element1, $house*5</p>
+
+<?php
+//CODIGO PHP
+$_myvar;
+$_7var; 
+//myvar; invalida
+$myvar; 
+$var7; 
+$_element1;
+//$house*5; invalida
+
+echo '<ul>';
+        echo '<li>$_myvar es válida porque inicia con guión bajo.</li>';
+        echo '<li>$_7var es válida porque inicia con guión bajo.</li>';
+        echo '<li>$myvar es válida porque inicia con una letra.</li>';
+        echo '<li>$var7 es válida porque inicia con una letra.</li>';
+        echo '<li>$_element1 es válida porque inicia con guión bajo.</li>';
+        echo '</ul>';
+
+?>
+
+</body>
+</html>
